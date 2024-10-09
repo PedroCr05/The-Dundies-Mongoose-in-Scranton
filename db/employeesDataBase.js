0fe095c6f5f85a99d0e90575bd2e45c49d2be820
pedrocruz <JayJMarquez05@gmail.com>
2024-10-09 13:08:46 -0500

    Main file for MongoDB code has been created. Hopefully it does not crash later.

---
+++ db/employeesDataBase.js
@@ -0,0 +1,18 @@
+const mongoose = require(`mongoose`);
+
+mongoose
+  .connect(
+    `mongodb+srv://PedroCr05:QzzDuBxSIezs9Mha@clusters.k4z0f.mongodb.net/the-dundies-mongoose-in-scranton?retryWrites=true&w=majority&appName=clusters`
+  )
+  .then(() => {
+    console.log(`Successfully connected to MongoDB.`);
+  })
+  .catch((e) => {
+    console.error(`Connection error`, e.message);
+  });
+
+mongoose.set(`debug`, true);
+
+const db = mongoose.connection;
+
+module.exports = db;
